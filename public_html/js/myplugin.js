199cc6a6198eaa14dfb9180f9050fcb1c3064cea
mohammad_mustafa_1995 <mohammad.e.mustafa@gmail.com>
2017-06-17 02:35:54 +0300

    first commit

---
+++ public_html/js/myplugin.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    /**************trigger nice scroll**************/
+    $("html").niceScroll({
+
+    });
+    /**************end trigger nice scroll**************/
+    //initialize swiper when document ready  
+    /*var mySwiper = new Swiper ('.swiper-container', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      speed:2000,
+      autoplay:5000
+    });
+    mySwiper.onClickNext(function (){
+        alert("next");
+    });
+    */
+})
